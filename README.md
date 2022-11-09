ee82158d0839c0f39b4b654803372af3ae2080d4
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2022-11-09 18:41:28 +0100

    adding index.js and package.json

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # FeurApi
 API for the 0b0t division of the FeurGroup
+
+# install
+
+you need node.js and npm.
+
+PM2 or screen-gnu is also recommanded but not mandatory.
+
+if you dont know how to install it, google it.
+
+install dependency : 
+
+`npm i`
+
+run the API (with pm2): 
+
+`pm2 start index.js`
+
+for nginx, just proxy_pass it (default port is 4200).
+
+# note
+
+The API by itself is not meant for web use. It is meant to be called through an hacked client like WaifuHax (private).
+
+The password in the index.js is not the one used in prod.
+
+Our current usage of that API is to run tpabots on 0b0t for the FeurGroup.
+
+0b0t is a minecraft anarchy server that have a /tpa command. We run headless minecraft instances that use a special hacked client (WaifuHax), that i'm making so the bots can survive on their own and auto tpy the players that have access to X base.
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require("body-parser");
+
+let db = new sqlite3.Database('./Players.db', sqlite3.OPEN_READWRITE);
+
+const api = express()
+
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.json());
+
+api.get('/member', (req, res) =>
+{
+
+	if(req.query.member === undefined){
+        let json = {
+            returnCode: 403,
+            message: "you forgot the query 'member'"
+        }
+
+        res.json(json)
+    }else{
+
+	db.each("SELECT * FROM Players WHERE UUID='" + req.query.member + "';", (err, row) => {
+		if(row.UUID === undefined){
+			let json = {
+                		returnCode: 404,
+		                message: "you are not a member of FeurGroup or whitelisted to one of our base, but you can join us to fix that mistake here : discord.gg/ReK658v4bs"
+            		}
+		        res.json(json)
+		}else{
+			let json = {
+                		returnCode: 200,
+                		message: row.UUID,
+	                	canAccessAllBases: row.canAccessAllBases,  
+		                assignedBase: row.base
+            		}
+
+			res.json(json)
+
+		}
+	});
+
+	}
+});
+
+
+api.post('/add', (req, res) => 
+{
+	
+	let message;
+
+	if(req.body.password == "password")
+	{
+		if(req.body.username === undefined){
+			message = "No username was given";	
+		}
+		else if(req.body.AllClear === undefined){
+			message = "no perms were given";
+		}else if(req.body.base === undefined) {
+			message = "no base were given";
+		}else{
+			message = "all of the data are clean, adding them to DB";
+			db.run("INSERT INTO Players VALUES('" + req.body.username + "', '"+ req.body.AllClear +"', '"+ req.body.base +"')");
+		}
+	}else{
+		message = "The given password is incorrect";
+	}
+	
+	let json = {
+		response: message
+	}
+	res.json(json);
+})
+
+/*
+player_db.insert({username : 'noctoack', canAccessToAllBases : 'no', assignedBase : 'degand'})
+player_db.insert({username : 'fox1', canAccessToAllBases : 'no', assignedBase : 'degand'})
+player_db.insert({username : 'painsaga', canAccessToAllBases : 'no', assignedBase : 'degand'})
+*/
+api.listen("4200", () => {
+    console.log("FeurAPI a été demarré, FeurGroup on TOP")
+})
+
